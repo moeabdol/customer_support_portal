dd71e5f5cc3cd0fb618ba24e7aded69a56645816
moeabdol <admin.r99@gmail.com>
2017-06-06 01:54:31 +0300

    Add Missing Test for home Controller GET #index Action

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
+    before(:example) { get :index }
+
     it "returns http success" do
-      get :index
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it 'renders index template' do
+      expect(response).to render_template(:index)
+    end
+  end
 end
